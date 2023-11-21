e98361cc592fa9815dc6664944248525c5990ba6
Daniel Esteban <conejo@conejo.me>
2023-11-21 10:03:05 +0100

    fix

--- generators/go/sensors.js
+++ generators/go/sensors.js
@@ -31,14 +31,14 @@ export function sensors_ds18b20_configure(block, generator) {
     return 'ow := onewire.New(machine.' + pin + ')\nromIDs, err := ow.Search(onewire.SEARCH_ROM)\nif err != nil {\nprintln(err)\n}\nsensors_ds18b20 := ds18b20.New(ow)\n';
 };
 
-export function sensors_sth4x_readtemperature(block, generator) {
+export function sensors_sht4x_readtemperature(block, generator) {
     generator.addImport('machine', 'machine');
     generator.addImport('tinygo.org/x/drivers/sht4x', 'tinygo.org/x/drivers/sht4x');
     generator.addVariable('sensors_sht4x', 'var sensors_sht4x sht4x.Device');
     generator.addDeclaration('gopherbadge_i2c', 'machine.I2C0.Configure(machine.I2CConfig{})\nsensors_sht4x = sht4x.New(machine.I2C0)\n');
     return 'sensors_sht4x.ReadTemperatureHumidity()\n';
 };
-export function sensors_sth4x_configure(block, generator) {
+export function sensors_sht4x_configure(block, generator) {
     generator.addImport('machine', 'machine');
     generator.addImport('tinygo.org/x/drivers/sht4x', 'tinygo.org/x/drivers/sht4x');
     generator.addVariable('sensors_sht4x', 'var sensors_sht4x sht4x.Device');
